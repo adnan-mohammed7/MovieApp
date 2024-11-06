6b63ff7b81c6afb64b6e949bd5aa68d8715c957f
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-11-06 10:57:56 -0500

    New Activities added

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.movieapp;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,16 +18,16 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Movie> movies = new ArrayList<>(Arrays.asList(
-            new Movie("Bodyguard", 2011, "m001", "Movie", "https://upload.wikimedia.org/wikipedia/en/a/af/Bodyguard_poster.jpg"),
-            new Movie("Ready", 2011, "m002", "Movie", "https://i.imgur.com/F7pr997.jpeg"),
-            new Movie("Veer", 2010, "m003", "Movie", "https://example.com/veer.jpg"),
-            new Movie("Kick", 2014, "m004", "Movie", "https://example.com/kick.jpg"),
-            new Movie("Ek Tha Tiger", 2012, "m005", "Movie", "https://example.com/ekthtiger.jpg"),
-            new Movie("Tiger Zinda Hai", 2017, "m006", "Movie", "https://example.com/tigerzindahai.jpg"),
-            new Movie("Wanted", 2009, "m007", "Movie", "https://example.com/wanted.jpg"),
-            new Movie("Radhe", 2021, "m008", "Movie", "https://example.com/radhe.jpg"),
-            new Movie("Antim: The Final Truth", 2021, "m009", "Movie", "https://example.com/antim.jpg"),
-            new Movie("Sultan", 2016, "m010", "Movie", "https://example.com/sultan.jpg")
+            new Movie("Bodyguard", 2011, "m001", "Movie", "https://i.imgur.com/vranu8L.jpeg"),
+            new Movie("Ready", 2011, "m002", "Movie", "https://i.imgur.com/xwNE49d.jpeg"),
+            new Movie("Veer", 2010, "m003", "Movie", "https://i.imgur.com/rVPdBqe.jpeg"),
+            new Movie("Kick", 2014, "m004", "Movie", "https://i.imgur.com/15YJ7Db.jpeg"),
+            new Movie("Ek Tha Tiger", 2012, "m005", "Movie", "https://i.imgur.com/ZSO9CzZ.jpeg"),
+            new Movie("Tiger Zinda Hai", 2017, "m006", "Movie", "https://i.imgur.com/KoQWSlS.jpeg"),
+            new Movie("Wanted", 2009, "m007", "Movie", "https://i.imgur.com/9esfKOF.jpeg"),
+            new Movie("Radhe", 2021, "m008", "Movie", "https://i.imgur.com/VrLyebX.jpeg"),
+            new Movie("Antim: The Final Truth", 2021, "m009", "Movie", "https://i.imgur.com/yrBmhZs.jpeg"),
+            new Movie("Sultan", 2016, "m010", "Movie", "https://i.imgur.com/PvjX7F0.jpeg")
     ));
 
     private RecyclerView recyclerView;
@@ -46,6 +48,21 @@ public class MainActivity extends AppCompatActivity {
                 createMovie();
             }
         });
+        fab.setEnabled(false);
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int pos = viewHolder.getLayoutPosition();
+                movies.remove(pos);
+                adapter.notifyItemRemoved(pos);
+            }
+        });
+        helper.attachToRecyclerView(recyclerView);
     }
     public void createMovie(){
 
